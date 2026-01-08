db9368f1ef5d8701f6ecfff712ebb1dd6f986bbc
Nithish8436 <nithish8436@gmail.com>
2026-01-08 22:48:34 +0530

    add versal speed insight

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import 'regenerator-runtime/runtime';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
   </StrictMode>,
 )
